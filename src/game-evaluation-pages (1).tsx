224a5c3ff2d0ff9ee4124b1a72c82f7b12d6dbf0
Anuj Rai <anujrai1701@gmail.com>
2024-11-06 09:28:18 +0530

    Added component folders and modules

--- src/game-evaluation-pages (1).tsx
+++ src/game-evaluation-pages (1).tsx
@@ -4,7 +4,7 @@ import { UserCircle, Users, Upload, Book, List, Hash, FileQuestion } from 'lucid
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
+import { Button } from "./components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 const GameEvaluationSystem = () => {
